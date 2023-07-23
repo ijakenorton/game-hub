5fd012d01ad173422dd1143bd6639865f9c1f3ae
Jake Norton <norja159@student.otago.ac.nz>
2023-07-23 19:24:04 +1200

    moved to its own repo for pushing to server

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { Grid, GridItem, HStack, Show, Flex, Box } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
+import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
+import PlatformSelector from "./components/PlatformSelector";
+import { useState } from "react";
+import { Genre } from "./hooks/useGenres";
+import { Platform } from "./hooks/usePlatforms";
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+  sortOrder: string;
+  searchText: string;
+}
+
+function App() {
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav"  "main"`,
+        lg: `"nav nav"  "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        ></NavBar>
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList
+            selectedGenre={gameQuery.genre}
+            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+          ></GenreList>
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <Box paddingLeft={2}>
+          <GameHeading gameQuery={gameQuery}></GameHeading>
+          <Flex  marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector
+                selectedPlatform={gameQuery.platform}
+                onSelectPlatform={(platform) =>
+                  setGameQuery({ ...gameQuery, platform })
+                }
+              ></PlatformSelector>
+            </Box>
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}
+              onSelectSortOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+            ></SortSelector>
+          </Flex>
+        </Box>
+        <GameGrid gameQuery={gameQuery}></GameGrid>
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
---
+++ src/components/GenreList.tsx
@@ -0,0 +1,54 @@
+import useGenres, { Genre } from "../hooks/useGenres";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
+
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
+}
+
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
+  const { isLoading, data, error } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner></Spinner>;
+  return (
+    <>
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              objectFit='cover'
+              src={getCroppedImageUrl(genre.image_background)}
+            ></Image>
+            <Button
+            textAlign='left'
+            whiteSpace='normal'
+              onClick={() => onSelectGenre(genre)}
+              variant="link"
+              fontSize="lg"
+              fontWeight={genre.id === selectedGenre?.id ? 'bold': 'normal'}
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+    </>
+  );
+};
+
+export default GenreList;
